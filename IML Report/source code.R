77114a58a523d155d53c61d7b022456f176a3787
muskuloes <t.victor@campus.lmu.de>
2020-12-11 22:15:53 +0100

    complete report

--- IML Report/source code.R
+++ IML Report/source code.R
@@ -1,4 +1,5 @@
 # Load necessary libraries
+library(data.table)
 library(mlr3)
 library(mlr3viz)
 library(mlr3learners)
@@ -11,13 +12,11 @@ library(paradox)
 
 library(ggplot2)
 library(GGally)
-library(data.table)
 library(ranger)
 library(xgboost)
 library(mboost)
 library(e1071)
 library(mltools)
-library(data.table)
 
 encode_ordinal <- function(x, order = unique(x)) {
   x <- as.numeric(factor(x, levels = order, exclude = NULL))
@@ -25,21 +24,27 @@ encode_ordinal <- function(x, order = unique(x)) {
 }
 
 load("../south-german-credit.Rda")
-data[which(sapply(data, is.ordered))] <- sapply(data[which(sapply(data, is.ordered))], encode_ordinal)
+data[which(sapply(data, is.ordered))] <- sapply(data[which(sapply(
+  data,
+  is.ordered
+))], encode_ordinal)
+
 cat_cols <- names(data)[which(sapply(data, is.factor))]
 cat_cols <- cat_cols[cat_cols != "credit_risk"]
 data <- data.table::as.data.table(data)
-data <- one_hot(data,cols=cat_cols)
+data <- one_hot(data, cols = cat_cols)
 names(data) <- make.names(names(data))
 int_cols <- names(data)[which(sapply(data, is.integer))]
-data[, (int_cols) := lapply(.SD, as.numeric), .SDcols=int_cols]
+data[, (int_cols) := lapply(.SD, as.numeric), .SDcols = int_cols]
 
-task <- TaskClassif$new("german_credit", data, target = "credit_risk", positive = "good")
+task <- TaskClassif$new("german_credit", data,
+  target = "credit_risk", positive = "good"
+)
 set.seed(11302020)
-classif_err_measure = msr("classif.ce")
-classif_xgboost_learner = lrn("classif.xgboost")
-tuner = tnr("grid_search", resolution = 5L)
-tune_ps = ParamSet$new(list(
+classif_err_measure <- msr("classif.ce")
+classif_xgboost_learner <- lrn("classif.xgboost")
+tuner <- tnr("grid_search", resolution = 5L)
+tune_ps <- ParamSet$new(list(
   ParamDbl$new("eta", lower = 0.01, upper = 0.3), # controls the learning rate
   ParamDbl$new("colsample_bytree", lower = 0.5, upper = 0.9), # fraction of features (variables) supplied to a tree
   ParamInt$new("max_depth", lower = 2, upper = 20), # maximum depth of the tree
@@ -49,8 +54,8 @@ tune_ps = ParamSet$new(list(
   ParamDbl$new("lambda", lower = 1, upper = 4.5), # L2 regularization
   ParamDbl$new("alpha", lower = 0, upper = 1) # L1 regularization
 ))
-terminator = trm("evals", n_evals = 5L)
-at = AutoTuner$new(
+terminator <- trm("evals", n_evals = 5L)
+at <- AutoTuner$new(
   learner = classif_xgboost_learner,
   resampling = rsmp("holdout"),
   measure = classif_err_measure,
@@ -60,12 +65,15 @@ at = AutoTuner$new(
 )
 
 
-design = benchmark_grid(
+design <- benchmark_grid(
   tasks = task,
   learners = list(lrn("classif.log_reg"), lrn("classif.rpart"), at),
   resamplings = rsmp("cv", folds = 5L)
-  # resamplings = rsmp("holdout", ratio=0.8)
 )
 
-bmr = benchmark(design)
-autoplot(bmr$filter(learner_ids = c("classif.log_reg", "classif.rpart", "classif.xgboost.tuned"))) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+bmr <- benchmark(design)
+autoplot(bmr$filter(learner_ids = c(
+  "classif.log_reg",
+  "classif.rpart", "classif.xgboost.tuned"
+))) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Interpretable-ML
+
+`rmarkdown::render("IML Report/IML Report.Rmd")`
