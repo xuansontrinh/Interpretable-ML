491776d3790d4c68f2bec5dafcaf168b9b7f2c7c
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2021-02-20 12:54:06 +0100

    Update source code for auc measure

--- IML Report/source code auc.R
+++ IML Report/source code auc.R
@@ -1,12 +1,14 @@
 library("mlr3viz")
 library("ggplot2")
 library("mlr3verse")
+library("paradox")
 set.seed(20211301)
 
 
 
 load("south-german-credit.Rda")
 task <- TaskClassif$new("german-credit", backend = data, target = "credit_risk", positive = "good")
+task$col_roles$stratum = "credit_risk"
 
 fencoder <- po("encode",
                method = "one-hot",
@@ -14,13 +16,18 @@ fencoder <- po("encode",
 )
 ord_to_num <- po("colapply",
                  applicator = as.numeric,
-                 affect_columns = selector_type(c("ordered","integer"))
+                 affect_columns = selector_type("ordered"),
+                 id = "ord_to_num"
 )
 
 int_to_num <- po("colapply",
                  applicator = as.numeric,
-                 affect_columns = selector_type("integer")
+                 affect_columns = selector_type("integer"),
+                 id = "int_to_num"
 )
+
+scaler <- po("scale",
+             affect_columns = selector_type(c("integer", "numeric")))
 
 # PipeOps
 # filter_op <- po("filter", flt("mim"),
@@ -30,65 +37,44 @@ po_over <- po("classbalancing",
               id = "oversample", adjust = "minor",
               reference = "minor", shuffle = FALSE, ratio = 2.3
 )
-pos <- po("scale") %>>%
-  fencoder %>>% ord_to_num %>>% po_over
+pos <- int_to_num %>>% scaler %>>% ord_to_num %>>% 
+  fencoder %>>% po_over
 
 inner_cv5 <- rsmp("cv", folds = 5L)
-measure <- msr("classif.bacc")
-# measure <- msr("classif.fbeta")
-tuner <- tnr("grid_search", resolution = 7L)
-# tuner <- tnr("random_search")
-terminator <- trm("evals", n_evals = 20)
+# measure <- msr("classif.bacc")
+measure <- msr("classif.auc")
+# measure <- msr("classif.bbrier")
+# tuner <- tnr("grid_search", resolution = 7L)
+tuner <- tnr("random_search")
+terminator <- trm("evals", n_evals = 40)
 
 
 
-log_reg_learner <- lrn("classif.log_reg", predict_type = "prob")
-log_reg_pipeline <- pos %>>% log_reg_learner %>>% po("threshold")
-log_reg_glearner <- GraphLearner$new(log_reg_pipeline, id = "log_reg")
-
-log_reg_at <- AutoTuner$new(
-  learner = log_reg_glearner,
-  resampling = inner_cv5,
-  measure = measure,
-  search_space = ParamSet$new(list(ParamDbl$new("threshold.thresholds", lower = 0, upper = 1))),
-  terminator = terminator,
-  tuner = tuner
-)
+log_reg_learner <- po(lrn("classif.log_reg", predict_type="prob"))
+log_reg_pipeline <- pos %>>% log_reg_learner
+log_reg_glearner <- GraphLearner$new(log_reg_pipeline, id = "log_reg", predict_type = "prob")
+# log_reg_glearner$predict_type = "prob"
 
 
-rpart_learner <- lrn("classif.rpart", predict_type = "prob")
-rpart_pipeline <- pos %>>% rpart_learner %>>% po("threshold")
-rpart_glearner <- GraphLearner$new(rpart_pipeline, id = "rpart")
 
-rpart_at <- AutoTuner$new(
-  learner = rpart_glearner,
-  resampling = inner_cv5,
-  measure = measure,
-  search_space = ParamSet$new(list(ParamDbl$new("threshold.thresholds", lower = 0, upper = 1))),
-  terminator = terminator,
-  tuner = tuner
-)
+rpart_learner <- po(lrn("classif.rpart", predict_type="prob"))
+rpart_pipeline <- pos %>>% rpart_learner
+rpart_glearner <- GraphLearner$new(rpart_pipeline, id = "rpart", predict_type = "prob")
+# rpart_glearner$predict_type = "prob"
 
-fct_rpart_pipeline <- po("scale") %>>% int_to_num %>>% po_over %>>% rpart_learner %>>% po("threshold")
-fct_rpart_glearner <- GraphLearner$new(fct_rpart_pipeline, id = 'fct_rpart')
-
-fct_rpart_at <- AutoTuner$new(
-  learner = fct_rpart_glearner,
-  resampling = inner_cv5,
-  measure = measure,
-  search_space = ParamSet$new(list(ParamDbl$new("threshold.thresholds", lower = 0, upper = 1))),
-  terminator = terminator,
-  tuner = tuner
-)
+fct_rpart_pipeline <- int_to_num %>>% scaler %>>% po_over %>>% rpart_learner
+fct_rpart_glearner <- GraphLearner$new(fct_rpart_pipeline, id = 'fct_rpart', predict_type = "prob")
+# fct_rpart_glearner$predict_type = "prob"
 
 
 
 
-ranger_pipeline <- pos %>>% lrn("classif.ranger", predict_type = "prob") %>>% po("threshold")
-ranger_glearner <- GraphLearner$new(ranger_pipeline, id='ranger')
+
+ranger_pipeline <- pos %>>% po(lrn("classif.ranger", predict_type="prob"))
+ranger_glearner <- GraphLearner$new(ranger_pipeline, id='ranger', predict_type = "prob")
+# ranger_glearner$predict_type = "prob"
 
 ranger_tune_ps <- ParamSet$new(list(
-  ParamDbl$new("threshold.thresholds", lower = 0, upper = 1),
   ParamInt$new("classif.ranger.num.trees", lower = 100, upper = 140), # number of trees
   ParamInt$new("classif.ranger.mtry", lower = 1, upper = ceiling(task$ncol / 2)), # number of variables to possibly split at in each node
   ParamInt$new("classif.ranger.max.depth", lower = 2, upper = 20) # maximum depth of the tree
@@ -103,8 +89,8 @@ ranger_at <- AutoTuner$new(
   tuner = tuner
 )
 
-fct_ranger_pipeline <- po("scale") %>>% int_to_num %>>% po_over %>>% lrn("classif.ranger", predict_type = "prob") %>>% po("threshold")
-fct_ranger_glearner <- GraphLearner$new(fct_ranger_pipeline, id='fct_ranger')
+fct_ranger_pipeline <- int_to_num %>>% scaler %>>% po_over %>>% po(lrn("classif.ranger", predict_type="prob"))
+fct_ranger_glearner <- GraphLearner$new(fct_ranger_pipeline, id='fct_ranger', predict_type = "prob")
 
 fct_ranger_at <- AutoTuner$new(
   learner = fct_ranger_glearner,
@@ -121,12 +107,11 @@ fct_ranger_at <- AutoTuner$new(
 
 
 
-xgboost_learner <- lrn("classif.xgboost", nrounds = 100, predict_type = "prob") %>>% po("threshold")
+xgboost_learner <- po(lrn("classif.xgboost", predict_type="prob"))
 xgboost_pipeline <- pos %>>% xgboost_learner
-xgboost_glearner <- GraphLearner$new(xgboost_pipeline, id="xg_boost")
+xgboost_glearner <- GraphLearner$new(xgboost_pipeline, id="xg_boost", predict_type = "prob")
 
 xgboost_search_space <- ParamSet$new(list(
-  ParamDbl$new("threshold.thresholds", lower = 0, upper = 1),
   ParamDbl$new("classif.xgboost.eta", lower = 0.1, upper = 0.3),
   ParamDbl$new("classif.xgboost.colsample_bytree", lower = 0.5, upper = 0.9),
   ParamInt$new("classif.xgboost.max_depth", lower = 8, upper = 16),
@@ -158,42 +143,39 @@ xgboost_at <- AutoTuner$new(
 
 
 
-linear_svm_learner <- lrn("classif.svm",
-                          type = "C-classification", kernel = "linear", predict_type = "prob"
-)
-poly_svm_learner <- lrn("classif.svm",
-                        type = "C-classification", kernel = "polynomial", predict_type = "prob"
-)
-radial_svm_learner <- lrn("classif.svm",
-                          type = "C-classification", kernel = "radial", predict_type = "prob"
-)
+linear_svm_learner <- po(lrn("classif.svm",
+                             type = "C-classification", kernel = "linear", predict_type="prob"
+))
+poly_svm_learner <- po(lrn("classif.svm",
+                           type = "C-classification", kernel = "polynomial", predict_type="prob"
+))
+radial_svm_learner <- po(lrn("classif.svm",
+                             type = "C-classification", kernel = "radial", predict_type="prob"
+))
 
 # Pipelines
-linear_svm_pipeline <- pos %>>% linear_svm_learner %>>% po("threshold")
-poly_svm_pipeline <- pos %>>% poly_svm_learner %>>% po("threshold")
-radial_svm_pipeline <- pos %>>% radial_svm_learner %>>% po("threshold")
+linear_svm_pipeline <- pos %>>% linear_svm_learner
+poly_svm_pipeline <- pos %>>% poly_svm_learner
+radial_svm_pipeline <- pos %>>% radial_svm_learner
 
 # Learners
-linear_svm_glearner <- GraphLearner$new(linear_svm_pipeline, id = "linear_svm")
-poly_svm_glearner <- GraphLearner$new(poly_svm_pipeline, id = "poly_svm")
-radial_svm_glearner <- GraphLearner$new(radial_svm_pipeline, id = "radial_svm")
+linear_svm_glearner <- GraphLearner$new(linear_svm_pipeline, id = "linear_svm", predict_type = "prob")
+poly_svm_glearner <- GraphLearner$new(poly_svm_pipeline, id = "poly_svm", predict_type = "prob")
+radial_svm_glearner <- GraphLearner$new(radial_svm_pipeline, id = "radial_svm", predict_type = "prob")
 
 # Search spaces
 poly_svm_search_space <- ParamSet$new(list(
-  ParamDbl$new("threshold.thresholds", lower = 0, upper = 1),
   ParamDbl$new("classif.svm.cost", lower = 0.01, upper = 100),
   ParamDbl$new("classif.svm.gamma", lower = 0.0001, upper = 1),
   ParamInt$new("classif.svm.degree", lower = 1, upper = 4)  
 ))
 
 radial_svm_search_space <- ParamSet$new(list(
-  ParamDbl$new("threshold.thresholds", lower = 0, upper = 1),
   ParamDbl$new("classif.svm.cost", lower = 0.01, upper = 100),
   ParamDbl$new("classif.svm.gamma", lower = 0.0001, upper = 1) 
 ))
 
 linear_svm_search_space <- ParamSet$new(list(
-  ParamDbl$new("threshold.thresholds", lower = 0, upper = 1),
   ParamDbl$new("classif.svm.cost", lower = 0.01, upper = 100)
 ))
 
@@ -230,9 +212,9 @@ outer_cv3 <- rsmp("cv", folds = 3L)
 design <- benchmark_grid(
   task = task,
   learners = list(
-    log_reg_at,
-    rpart_at,
-    fct_rpart_at,
+    log_reg_glearner,
+    rpart_glearner,
+    fct_rpart_glearner,
     ranger_at,
     fct_ranger_at,
     xgboost_at,
@@ -254,3 +236,5 @@ autoplot(bmr, measure = msr("classif.fbeta")) + theme(axis.text.x = element_text
 autoplot(bmr, measure = msr("classif.bacc")) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 autoplot(bmr, measure = msr("classif.precision")) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 autoplot(bmr, measure = msr("classif.recall")) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+autoplot(bmr, measure = msr("classif.auc")) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+autoplot(bmr, measure = msr("classif.bbrier")) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
